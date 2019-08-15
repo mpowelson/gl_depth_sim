7f47e8feb49e673ad1cf4bd7ba2b904a76af5c1c
Matthew Powelson <matthew.powelson@swri.org>
2019-08-15 15:57:56 -0500

    Add construct shader from file

--- include/gl_depth_sim/shader_program.h
+++ include/gl_depth_sim/shader_program.h
@@ -6,23 +6,25 @@
 
 namespace gl_depth_sim
 {
-
 class ShaderProgram
 {
 public:
+  ShaderProgram(const std::string& vertex_shader_path, const std::string& frag_shader_path, const bool& extreme_mode);
   ShaderProgram(const std::string& vertex_shader, const std::string& frag_shader);
   ~ShaderProgram();
+
+  void init(const std::string& vertex_shader, const std::string& frag_shader);
 
   unsigned int id() const { return id_; }
 
   // Interaction with attributes
   void setInt(const std::string& attr, int val);
-  void setUniformMat4(const std::string &attr, const Eigen::Matrix4f& mat);
+  void setUniformMat4(const std::string& attr, const Eigen::Matrix4f& mat);
 
 private:
   unsigned int id_;
 };
 
-}
+}  // namespace gl_depth_sim
 
 #endif
--- src/gl_depth_sim/shader_program.cpp
+++ src/gl_depth_sim/shader_program.cpp
@@ -1,16 +1,17 @@
 #include "gl_depth_sim/shader_program.h"
 #include <gl_depth_sim/glad/glad.h>
 #include <iostream>
+#include <fstream>
 #include <vector>
 
-gl_depth_sim::ShaderProgram::ShaderProgram(const std::string& vertex_shader, const std::string& frag_shader)
+void gl_depth_sim::ShaderProgram::init(const std::string& vertex_shader, const std::string& frag_shader)
 {
   GLuint vshader = glCreateShader(GL_VERTEX_SHADER);
   const auto* vtext = vertex_shader.c_str();
   glShaderSource(vshader, 1, &vtext, NULL);
   glCompileShader(vshader);
 
-  std::vector<char> log (1024, 0);
+  std::vector<char> log(1024, 0);
   int success;
   glGetShaderiv(vshader, GL_COMPILE_STATUS, &success);
 
@@ -50,6 +51,56 @@ gl_depth_sim::ShaderProgram::ShaderProgram(const std::string& vertex_shader, con
   }
   glDeleteShader(vshader);
   glDeleteShader(fshader);
+}
+
+gl_depth_sim::ShaderProgram::ShaderProgram(const std::string& vertex_shader_path,
+                                           const std::string& frag_shader_path,
+                                           const bool& extreme_mode)
+{
+  if (extreme_mode)
+    std::cout << "Executing in extreme mode!" << std::endl;
+
+  // These will contain the shader code
+  std::string vertex_shader;
+  std::string frag_shader;
+
+  // Set up ifstreams
+  std::ifstream vertex_shader_ifstream;
+  std::ifstream frag_shader_ifsteam;
+  vertex_shader_ifstream.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+  frag_shader_ifsteam.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+  try
+  {
+    // Open the files
+    vertex_shader_ifstream.open(vertex_shader_path);
+    frag_shader_ifsteam.open(frag_shader_path);
+
+    // Read file into a string stream
+    std::stringstream vertex_shader_string_stream;
+    std::stringstream fragment_shader_string_stream;
+    vertex_shader_string_stream << vertex_shader_ifstream.rdbuf();
+    fragment_shader_string_stream << frag_shader_ifsteam.rdbuf();
+
+    // Close the files
+    vertex_shader_ifstream.close();
+    frag_shader_ifsteam.close();
+
+    // Convert the stream into a string
+    vertex_shader = vertex_shader_string_stream.str();
+    frag_shader = fragment_shader_string_stream.str();
+  }
+  catch (std::ifstream::failure e)
+  {
+    std::cerr << "Shader file has not been correcly read:" << std::endl;
+  }
+
+  // Send to init
+  init(vertex_shader, frag_shader);
+}
+
+gl_depth_sim::ShaderProgram::ShaderProgram(const std::string& vertex_shader, const std::string& frag_shader)
+{
+  init(vertex_shader, frag_shader);
 }
 
 gl_depth_sim::ShaderProgram::~ShaderProgram()
